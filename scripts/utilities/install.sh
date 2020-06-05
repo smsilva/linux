dfbc6da1f887babb60f8059260d4827cc9006d55
Silvio Silva <smsilva@gmail.com>
2020-06-05 16:51:07 -0300

    Configura o today-fortune no ~/.bashrc

--- scripts/utilities/install.sh
+++ scripts/utilities/install.sh
@@ -19,3 +19,10 @@ sudo apt-get install -y \
 scripts/utilities/powerline/install.sh
 scripts/utilities/tmux/install.sh
 scripts/utilities/fuzzy-finder/install.sh
+
+if ! grep --quiet --extended-regexp "today-fortune" ~/.bashrc; then
+cat <<EOF >> ~/.bashrc
+
+today-fortune
+EOF
+fi

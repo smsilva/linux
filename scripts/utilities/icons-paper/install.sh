68d748aeb4a4bed604f40243b298388de703a7cd
Silvio Silva <smsilva@gmail.com>
2020-10-03 11:25:38 -0300

    Instalacao do jq, yq, icones paper e configuracao da Fonte Menlo for Powerline no VS Code

---
+++ scripts/utilities/icons-paper/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo add-apt-repository ppa:snwh/ppa --yes
+
+# update repository info
+sudo apt-get update
+
+# install icon theme
+sudo apt-get install paper-icon-theme 
--- scripts/utilities/powerline/install.sh
+++ scripts/utilities/powerline/install.sh
@@ -28,3 +28,21 @@ python3 powerline_setup()
 python3 del powerline_setup
 set laststatus=2
 EOF
+
+# Set the VS Code Terminal Font
+VSCODE_USER_SETTINGS_FILE="${HOME}/.config/Code/User/settings.json"
+FONT_FILE_NAME="Menlo for Powerline.ttf" && \
+JQ_EXPRESSION=$(printf '."terminal.integrated.fontFamily" = "%s"' "${FONT_FILE_NAME%.ttf}") && \
+echo "" && \
+echo "VSCODE_USER_SETTINGS_FILE...: ${VSCODE_USER_SETTINGS_FILE}" && \
+echo "FONT_FILE_NAME..............: ${FONT_FILE_NAME}" && \
+echo "JQ_EXPRESSION...............: ${JQ_EXPRESSION}" && \
+echo ""
+
+sudo wget -qO "/usr/share/fonts/${FONT_FILE_NAME}" "https://github.com/abertsch/Menlo-for-Powerline/raw/master/Menlo%20for%20Powerline.ttf"
+
+if ! [ -e "${VSCODE_USER_SETTINGS_FILE}" ]; then
+  echo '{}' > "${VSCODE_USER_SETTINGS_FILE}"
+fi
+
+echo "$(jq "${JQ_EXPRESSION}" "${VSCODE_USER_SETTINGS_FILE}")" > "${VSCODE_USER_SETTINGS_FILE}"

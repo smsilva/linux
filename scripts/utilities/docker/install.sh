2cae31fcf5151fd96388788851df4e6dc5a496b1
Silvio Silva <smsilva@gmail.com>
2020-10-03 12:57:42 -0300

    Instalação do Docker e do Kubectl

---
+++ scripts/utilities/docker/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo apt-get remove \
+  docker \
+  docker-engine \
+  docker.io \
+  containerd runc
+
+sudo apt-get update -q
+
+sudo apt-get install -q \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common \
+  --yes
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -q
+
+sudo apt-get install -q \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  --yes
+
+sudo usermod -aG docker ${USER}
+
+sudo systemctl enable docker
+sudo systemctl start docker

46255877bf859390f4744f42cc0c03d9470ab6b2
Silvio Silva <smsilva@gmail.com>
2021-06-24 07:59:54 -0300

    Aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,123 +1,103 @@
 alias aal='az account list -o table'
 alias agl='az group list -o table'
 alias aksl='az aks list -o table'
-
 alias bat='batcat'
 alias cat='bat -p'
 alias clip="xclip -selection clipboard"
-alias ct='column -t'
 alias csvt='. csvt'
-
-alias f="fdfind"
-alias fa="fdfind -a"
-alias faf="fdfind -a -t f"
-alias tfc="tfclear"
-alias fd="fdfind"
-alias ff="fd -t f"
+alias ct='column -t'
+alias fa="fd -a"
+alias faf="fd -a -t f"
 alias fdd="fd -t d"
+alias f="fd"
+alias ff="fd -t f"
+alias fi="fd -t f -I"
+alias fpa="fd --type f --hidden --follow --exclude .git | fzf -m --preview 'bat --style=numbers --color=always {} | head -500'"
 alias fp="fzf -m --preview 'batcat --style=numbers --color=always {} | head -500'"
-alias fpa="fdfind --type f --hidden --follow --exclude .git | fzf -m --preview 'bat --style=numbers --color=always {} | head -500'"
-
-alias tg='terragrunt'
-
-alias ht='helm template .'
-alias hta='helm template . | kubectl apply -f -'
-
-alias i='istioctl'
-alias k='kubectl'
-alias ka='kubectl -n argocd'
-alias ki='kubectl -n istio-system'
-alias ko='kubectl -n istio-operator'
-alias ks='kubectl -n kube-system'
-alias kap='kubectl apply -f'
-alias kgn='kubectl get nodes'
-alias kpo='kubectl get pods'
-
-alias l1="ls -1 --color=auto"
-alias la="ls -la"
-alias lc="ls | column -c 1"
-alias lh="ls -lh"
-alias lha="ls -lha"
-
-alias lower='tr "[:upper:]" "[:lower:]"'
-alias upper='tr "[:lower:]" "[:upper:]"'
-
-alias mem='free -h'
-
-alias mpalias="alias | grep -E '(^alias).*(multipass).*' | sed 's/alias //' | column -t -s '=' | tr -d \' | sed '/^mpalias/ d'"
-alias mp='multipass'
-alias mph='multipass stop'
-alias mpl='multipass list'
-alias mps='multipass start'
-alias mpsu='multipass suspend'
-
-alias os='microstack.openstack'
-
-alias pwc='echo $(pwd) && echo -n $(pwd) | clip'
-
-alias tx='tmux'
-alias txn='tmux new -s "local"'
-
-alias tf='terraform'
-alias tfi='terraform init'
-alias tfip='terraform init && terraform plan -out .terraform.plan'
-alias tfp='terraform plan -out .terraform.plan'
-alias tfa='terraform apply -auto-approve .terraform.plan'
-alias tfar='terraform apply -auto-approve -refresh-only'
-alias tfd='terraform destroy -auto-approve'
-
-alias tg='terragrunt'
-alias tgi='terragrunt run-all init'
-alias tga='terragrunt run-all apply --terragrunt-non-interactive'
-alias tgad='terragrunt run-all apply --terragrunt-non-interactive --terragrunt-log-level debug --terragrunt-debug'
-alias tgar='terragrunt run-all apply --terragrunt-non-interactive -refresh-only'
-alias tgp='terragrunt run-all plan'
-alias tgd='terragrunt run-all destroy'
-
+alias ga='git add'
 alias galias="alias | grep 'alias g' | grep -P '(^alias )\K.*' -o | sed 's/=/\t/g' | tr -d \' | grep -v 'galias' | grep -v 'grep' | sort | fzf"
-
-alias gb='git branch'
-alias gbn='git checkout -b'
-alias gbd='git branch -d $(git branch | fzf)'
 alias gbdf='git branch -d -f $(git branch | fzf)'
+alias gbd='git branch -d $(git branch | fzf)'
+alias gb='git branch'
 alias gbl='git branch'
 alias gblr='git branch -r'
+alias gbn='git checkout -b'
+alias gcm='git commit -m'
 alias gcob='git checkout --track $(git branch -r | fzf)'
 alias gdiff='git diff'
 alias gfp='git fetch --prune'
-alias gp='git pull'
-alias gpr='git pull --rebase'
-alias gst='git status'
-alias gpf='git push --force'
 alias gl='git log'
 alias glol='git log --oneline'
-alias ga='git add'
-alias gcm='git commit -m'
-alias grh='git reset --hard'
-alias gr='git rebase'
-alias grc='git rebase --continue'
+alias gpf='git push --force'
+alias gp='git pull'
+alias gpr='git pull --rebase'
+alias gpushf='git push --force'
+alias gpush='git push'
+alias gpushsu='git push --set-upstream origin $(git branch --list | grep -Po "\* \K.*")'
 alias gra='git rebase --abort'
-alias gt='git tag'
+alias grc='git rebase --continue'
+alias gr='git rebase'
+alias grh='git reset --hard'
+alias gst='git status'
 alias gtd='git tag -d'
 alias gtdr='git push --delete origin'
+alias gt='git tag'
 alias gtl='git tag -l'
 alias gtr='gretag'
-alias gpush='git push'
-alias gpushf='git push --force'
-alias gpushsu='git push --set-upstream origin $(git branch --list | grep -Po "\* \K.*")'
-
-alias vg='vagrant'
-alias vgs='vagrant status' 
-alias vgu='vagrant up'
+alias hta='helm template . | kubectl apply -f -'
+alias ht='helm template .'
+alias i='istioctl'
+alias ka='kubectl -n argocd'
+alias kap='kubectl apply -f'
+alias kgn='kubectl get nodes'
+alias ki='kubectl -n istio-system'
+alias k='kubectl'
+alias ko='kubectl -n istio-operator'
+alias kpo='kubectl get pods'
+alias ks='kubectl -n kube-system'
+alias l1="ls -1 --color=auto"
+alias la="ls -la"
+alias lc="ls | column -c 1"
+alias lha="ls -lha"
+alias lh="ls -lh"
+alias lower='tr "[:upper:]" "[:lower:]"'
+alias mem='free -h'
+alias mpalias="alias | grep -E '(^alias).*(multipass).*' | sed 's/alias //' | column -t -s '=' | tr -d \' | sed '/^mpalias/ d'"
+alias mph='multipass stop'
+alias mpl='multipass list'
+alias mp='multipass'
+alias mps='multipass start'
+alias mpsu='multipass suspend'
+alias now='date +"%Y-%m-%d %H:%M:%S"'
+alias os='microstack.openstack'
+alias pwc='echo $(pwd) && echo -n $(pwd) | clip'
+alias tfar='terraform apply -auto-approve -refresh-only'
+alias tfa='terraform apply -auto-approve .terraform.plan'
+alias tfc="tfclear"
+alias tfd='terraform destroy -auto-approve'
+alias tfip='terraform init && terraform plan -out .terraform.plan'
+alias tfi='terraform init'
+alias tfp='terraform plan -out .terraform.plan'
+alias tf='terraform'
+alias tfvars='ln -s ~/.terraform.tfvars terraform.tfvars'
+alias tgad='terragrunt run-all apply --terragrunt-non-interactive --terragrunt-log-level debug --terragrunt-debug'
+alias tgar='terragrunt run-all apply --terragrunt-non-interactive -refresh-only'
+alias tga='terragrunt run-all apply --terragrunt-non-interactive'
+alias tgd='terragrunt run-all destroy'
+alias tgi='terragrunt run-all init'
+alias tgp='terragrunt run-all plan'
+alias tg='terragrunt'
+alias trans='trans -t pt --brief --no-auto'
+alias t='tree'
+alias txn='tmux new -s "local"'
+alias tx='tmux'
+alias upper='tr "[:lower:]" "[:upper:]"'
 alias vgd='vagrant destroy -f'
 alias vgh='vagrant halt'
-alias vgsu='vagrant suspend'
 alias vgr='vagrant reload'
-
-alias now='date +"%Y-%m-%d %H:%M:%S"'
-alias trans='trans -t pt --brief --no-auto'
-
-alias ytop='ytop -c monokai'
+alias vgsu='vagrant suspend'
+alias vgs='vagrant status' 
+alias vgu='vagrant up'
+alias vg='vagrant'
 alias yq='yq -C'
-
-alias tfvars='ln -s ~/.terraform.tfvars terraform.tfvars'
+alias ytop='ytop -c monokai'

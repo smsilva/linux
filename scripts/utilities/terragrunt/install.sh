fd0e54261333a178abdf6c518c4ca56896e1289d
Silvio Silva <smsilva@gmail.com>
2021-06-25 01:06:45 -0300

    Instalando versao mais nova do terragrunt

---
+++ scripts/utilities/terragrunt/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+LATEST_GITHUB_RELEASE_VERSION=$(curl -sL https://github.com/gruntwork-io/terragrunt/releases | grep -oP 'releases/tag/\K[^\"]+' | sort --version-sort | tail -1)
+
+echo "downloading terragrunt: ${LATEST_GITHUB_RELEASE_VERSION?}"
+
+wget --quiet https://github.com/gruntwork-io/terragrunt/releases/download/${LATEST_GITHUB_RELEASE_VERSION?}/terragrunt_linux_amd64
+
+chmod +x terragrunt_linux_amd64
+
+mkdir -p ${HOME?}/bin
+
+mv terragrunt_linux_amd64 ${HOME?}/bin/terragrunt
+
+terragrunt -version

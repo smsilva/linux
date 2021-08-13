c1019bdc223c86db401ed645a77b7f31c1b3a1eb
Silvio Silva <smsilva@gmail.com>
2021-08-13 18:50:16 -0300

    Alias tfs para tfswitch

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -14,6 +14,7 @@ alias fdd="fdfind -t d"
 alias ff="fdfind -t f"
 alias fp="fzf -m --preview 'batcat --style=numbers --color=always {} | head -500'"
 alias fpa="fdfind --type f --hidden --follow --exclude .git | fzf -m --preview 'bat --style=numbers --color=always {} | head -500'"
+alias tfs='tfswitch'
 alias ga='git add'
 alias galias="alias | grep 'alias g' | grep -P '(^alias )\K.*' -o | sed 's/=/\t/g' | tr -d \' | grep -v 'galias' | grep -v 'grep' | sort | fzf"
 alias gb='git branch'
---
+++ scripts/utilities/tfswitch/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+if ! which tfswitch > /dev/null; then
+  curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | sudo bash
+fi
+
+tfswitch --version

0e69eed73fd0072ace231c00759c53dcc135de71
Silvio Silva <smsilva@gmail.com>
2021-10-23 10:53:21 -0300

    Instalando yq

--- scripts/utilities/yq/install.sh
+++ scripts/utilities/yq/install.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 if ! which yq > /dev/null; then
-  sudo snap install yq
+  VERSION="4.13.5"
+  BINARY="yq_linux_amd64"
+  TAR_FILE="${BINARY?}.tar.gz"
+
+  echo "VERSION..: ${VERSION}"
+  echo "BINARY...: ${BINARY}"
+  echo "TAR_FILE.: ${TAR_FILE}"
+
+  if [ ! -e "${TAR_FILE?}" ]; then
+    wget https://github.com/mikefarah/yq/releases/download/v${VERSION?}/${TAR_FILE?}
+  fi
+
+  tar xvf "${TAR_FILE?}" "./${BINARY?}"
+
+  if [ -e "${BINARY?}" ]; then
+    mkdir -p "${HOME}/bin"
+    mv ${BINARY} "${HOME}/bin/yq"
+    rm "${TAR_FILE?}"
+  fi
+
+  yq --version
 fi

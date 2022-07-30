ab76d07a1c4140a3934595140828193b93428843
Silvio Silva <smsilva@gmail.com>
2022-07-30 09:53:55 -0300

    feat(scripts/bin/dns-cleanup.sh): Script creation

---
+++ scripts/bin/dns-cleanup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ENVIRONMENT_ID="$1"
+DNS_ZONE_NAME="sandbox.wasp.silvios.me"
+DNS_ZONE_RESOURCE_GROUP_NAME="wasp-foundation"
+DNS_ZONE_RECORDS_FILE="$(tempfile)"
+
+az network dns record-set a list \
+  --resource-group "${DNS_ZONE_RESOURCE_GROUP_NAME?}" \
+  --zone-name ${DNS_ZONE_NAME?} \
+  --output table >> ${DNS_ZONE_RECORDS_FILE?}
+
+az network dns record-set txt list \
+  --resource-group "${DNS_ZONE_RESOURCE_GROUP_NAME?}" \
+  --zone-name ${DNS_ZONE_NAME?} \
+  --output table >> ${DNS_ZONE_RECORDS_FILE?}
+
+grep ${ENVIRONMENT_ID?} ${DNS_ZONE_RECORDS_FILE?} | sort -u | awk '{ print $1, $4 }' | while read LINE; do
+  RECORD_NAME=$(awk '{ print $1 }' <<< "${LINE}")
+  RECORD_TYPE=$(awk '{ print $2 }' <<< "${LINE}")
+
+  echo "RECORD_TYPE=${RECORD_TYPE} RECORD_NAME=${RECORD_NAME}"
+  
+  az network dns record-set ${RECORD_TYPE,,} delete \
+    --name "${RECORD_NAME?}" \
+    --resource-group "${DNS_ZONE_RESOURCE_GROUP_NAME?}" \
+    --zone-name ${DNS_ZONE_NAME?} \
+    --yes \
+    --only-show-errors
+done

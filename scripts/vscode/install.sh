c1c5669ee3df546e59100cbacaf726e65d1c2ffb
Silvio Silva <smsilva@gmail.com>
2022-08-11 08:09:15 -0300

    feat: Update to be compatible with Ubuntu 22.04.1 LTS

--- scripts/vscode/install.sh
+++ scripts/vscode/install.sh
@@ -9,7 +9,7 @@ if ! which code &> /dev/null; then
     apt-transport-https \
     wget 
 
-  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 
   sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 

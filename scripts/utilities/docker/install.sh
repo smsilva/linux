9440be426e373e9560788d22182fa7bf477f51fb
Silvio Silva <smsilva@gmail.com>
2023-04-18 09:46:51 -0300

    feat(scripts/utilities/docker/install.sh): Install Docker Compose

--- scripts/utilities/docker/install.sh
+++ scripts/utilities/docker/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if ! which docker > /dev/null; then
   sudo apt-get remove \
+    containerd \
     docker \
+    docker-compose-plugin \
     docker-engine \
     docker.io \
-    containerd runc
+    runc
 
   sudo apt-get update -q
 
@@ -16,19 +18,24 @@ if ! which docker > /dev/null; then
     software-properties-common \
     --yes
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.gpg
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-  sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   sudo apt-get update -q
 
   sudo apt-get install -q \
+    containerd.io \
+    docker-buildx-plugin \
     docker-ce \
     docker-ce-cli \
-    containerd.io \
+    docker-compose-plugin \
     --yes
 
   sudo usermod -aG docker ${USER}

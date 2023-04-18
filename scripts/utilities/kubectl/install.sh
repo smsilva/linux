a3fe3b8fc2515fbb3da8f43b16e3d52a6a9d3c36
Silvio Silva <smsilva@gmail.com>
2023-04-18 11:01:07 -0300

    feat(scripts/utilities/kubectl/install.sh): Update

--- scripts/utilities/kubectl/install.sh
+++ scripts/utilities/kubectl/install.sh
@@ -10,7 +10,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-  sudo apt-get update -q
+  sudo apt-get update -qqq
 
   sudo apt-get install kubectl=1.24.13-00 --yes -q
 

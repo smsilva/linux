d1669e848bed24c3de6c7b4a9179aed02ddf294a
Silvio Silva <smsilva@gmail.com>
2023-04-18 10:53:40 -0300

    feat(scripts/utilities/kubectl/install.sh): Update

--- scripts/utilities/kubectl/install.sh
+++ scripts/utilities/kubectl/install.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
 if ! which kubectl > /dev/null; then
-  # Update and Get Google Cloud Apt Key
-  sudo apt-get update -qq && \
-  wget -qO- "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo tee /etc/apt/trusted.gpg.d/google-cloud.gpg
-  
-# Add Kubernetes Repository
+  sudo curl \
+    --location \
+    --output "/usr/share/keyrings/kubernetes-archive-keyring.gpg" \
+    "https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-  
-  # Update package list
+
   sudo apt-get update -q
-  
-  # Set Kubernetes Version
-  KUBERNETES_VERSION="$(apt-cache madison kubeadm | head -1 | awk '{ print $3 }')" && \
-  KUBERNETES_IMAGE_VERSION="${KUBERNETES_VERSION%-*}" && \
-  clear && \
-  echo "" && \
-  echo "KUBERNETES_VERSION.........: ${KUBERNETES_VERSION}" && \
-  echo "KUBERNETES_IMAGE_VERSION...: ${KUBERNETES_IMAGE_VERSION}" && \
-  echo ""
-  
-  sudo apt-get install --yes -qq \
-    kubectl="${KUBERNETES_VERSION}" | grep --invert-match --extended-regexp "^Hit|^Get|^Selecting|^Preparing|^Unpacking"
+
+  sudo apt-get install kubectl=1.24.13-00 --yes -q
+
+  sudo apt-mark hold kubectl
 fi

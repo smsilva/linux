3ac8c9d91e391617ee4cdf3a966499c824b39b80
Silvio Silva <smsilva@gmail.com>
2023-11-10 09:40:34 -0300

    feat: Update install scripts

--- scripts/sudoers/setup.sh
+++ scripts/sudoers/setup.sh
@@ -4,5 +4,6 @@ CURRENT_USERNAME=$1
 echo "Configuring /etc/sudoers.d/${CURRENT_USERNAME} sudoer file"
 
 if [[ ! -e /etc/sudoers.d/${CURRENT_USERNAME} ]]; then
-  echo "${CURRENT_USERNAME} ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/${CURRENT_USERNAME}
+  echo "${CURRENT_USERNAME} ALL=(ALL) NOPASSWD: ALL" \
+  | sudo tee /etc/sudoers.d/${CURRENT_USERNAME}
 fi

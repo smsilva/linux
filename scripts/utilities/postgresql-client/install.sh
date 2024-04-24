735443adfe4b5392b6bc6089ef514249ccbdd486
Silvio Silva <smsilva@gmail.com>
2024-04-24 11:04:08 -0300

    feat(scripts/utilities/postgresql-client/install.sh): Update

--- scripts/utilities/postgresql-client/install.sh
+++ scripts/utilities/postgresql-client/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if ! which psql > /dev/null; then
-  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+  wget --quiet --output-document - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
   | gpg --dearmor \
   | sudo tee /usr/share/keyrings/postgresql-archive-keyring.gpg
   
-  echo "deb [signed-by=/usr/share/keyrings/postgresql-archive-keyring.gpg] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" \
-  | sudo tee /etc/apt/sources.list.d/pgdg.list
-  
+  cat <<EOF | sudo tee /etc/apt/sources.list.d/pgdg.list
+deb [signed-by=/usr/share/keyrings/postgresql-archive-keyring.gpg] https://apt.postgresql.org/pub/repos/apt $(lsb_release --codename --short)-pgdg main
+EOF
+ 
   sudo apt-get update
 
   sudo apt-get install \

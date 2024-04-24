d42cb856cf2803a0ee2baa37475a95f5f3ae8b08
Silvio Silva <smsilva@gmail.com>
2024-04-24 11:09:51 -0300

    feat(scripts/utilities/yq/install.sh): Update from version 4.35.1 to 4.43.1

--- scripts/utilities/yq/install.sh
+++ scripts/utilities/yq/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! which yq > /dev/null; then
-  VERSION="4.35.1"
+  VERSION="4.43.1"
   BINARY="yq_linux_amd64"
   TAR_FILE="${BINARY?}.tar.gz"
 
@@ -8,13 +8,13 @@ if ! which yq > /dev/null; then
   echo "BINARY...: ${BINARY}"
   echo "TAR_FILE.: ${TAR_FILE}"
 
-  if [ ! -e "${TAR_FILE?}" ]; then
+  if [[ ! -e "${TAR_FILE?}" ]]; then
     wget https://github.com/mikefarah/yq/releases/download/v${VERSION?}/${TAR_FILE?}
   fi
 
   tar xvf "${TAR_FILE?}" "./${BINARY?}"
 
-  if [ -e "${BINARY?}" ]; then
+  if [[ -e "${BINARY?}" ]]; then
     mkdir -p "${HOME}/bin"
     mv ${BINARY} "${HOME}/bin/yq"
     rm "${TAR_FILE?}"

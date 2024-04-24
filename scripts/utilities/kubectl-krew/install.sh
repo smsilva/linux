e88746b30fae156479a4fde855cf6e042cba67b8
Silvio Silva <smsilva@gmail.com>
2024-04-24 10:55:07 -0300

    feat(scripts/utilities/kubectl-krew/install.sh): Update

--- scripts/utilities/kubectl-krew/install.sh
+++ scripts/utilities/kubectl-krew/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 INSTALL_DIR="${HOME}/.krew"
 
-if ! [ -e "${INSTALL_DIR}" ]; then
+if [[ ! -e "${INSTALL_DIR?}" ]]; then
   (
     set -x; cd "$(mktemp -d)" &&
     curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.4.4/krew-linux_amd64.tar.gz" &&
     tar zxvf krew-linux_amd64.tar.gz &&
     KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
-    "$KREW" install krew
+    "${KREW}" install krew
   )
 fi
 
 if ! grep --quiet --extended-regexp "^export.*KREW_ROOT.*PATH" ~/.bashrc; then
-  echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
+  echo 'export PATH="${KREW_ROOT:-${HOME}/.krew}/bin:${PATH}"' >> ~/.bashrc
 
   kubectl krew update
 

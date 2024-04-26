50a0b6eadfc246043a8c44a9b8c8c743081bda7a
Silvio Silva <smsilva@gmail.com>
2024-04-26 08:58:00 -0300

    fix(scripts/sudoers/setup.sh): Installation check

--- scripts/sudoers/setup.sh
+++ scripts/sudoers/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 CURRENT_USERNAME=$1
+SUDOER_FILE="/etc/sudoers.d/${CURRENT_USERNAME}"
 
-echo "Configuring /etc/sudoers.d/${CURRENT_USERNAME} sudoer file"
+if [[ ! -e "${SUDOER_FILE?}" ]]; then
+  echo "Creating ${SUDOER_FILE?} file:"
 
-if [[ ! -e /etc/sudoers.d/${CURRENT_USERNAME} ]]; then
-  echo "${CURRENT_USERNAME} ALL=(ALL) NOPASSWD: ALL" \
-  | sudo tee /etc/sudoers.d/${CURRENT_USERNAME}
+  cat << EOF | sudo tee ${SUDOER_FILE?}
+${CURRENT_USERNAME} ALL=(ALL) NOPASSWD: ALL
+EOF
+
 fi

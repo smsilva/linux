9dcab350c5905920d22be2e9364f9da212e0ab69
Silvio Silva <smsilva@gmail.com>
2024-07-18 13:15:08 -0300

    feat: new command and update yq install

--- scripts/utilities/yq/install.sh
+++ scripts/utilities/yq/install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 if ! which yq > /dev/null; then
-  VERSION="4.44.1"
   BINARY="yq_linux_amd64"
   TAR_FILE="${BINARY?}.tar.gz"
+  VERSION=$(curl \
+    --silent "https://api.github.com/repos/mikefarah/yq/releases/latest" \
+    | grep '"tag_name"' \
+    | sed -E 's|(.*")([^"]+)(".*)|\2|'
+  )
 
   echo "VERSION..: ${VERSION}"
   echo "BINARY...: ${BINARY}"
   echo "TAR_FILE.: ${TAR_FILE}"
 
   if [[ ! -e "${TAR_FILE?}" ]]; then
-    wget https://github.com/mikefarah/yq/releases/download/v${VERSION?}/${TAR_FILE?}
+    wget https://github.com/mikefarah/yq/releases/download/${VERSION?}/${TAR_FILE?}
   fi
 
   tar xvf "${TAR_FILE?}" "./${BINARY?}"

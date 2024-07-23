84c3b8ad33faf44c70fde8a2466d7e92451ff9dc
Silvio Silva <smsilva@gmail.com>
2024-07-23 09:04:04 -0300

    refactor(scripts/utilities/postgresql-client/install.sh): use variables

--- scripts/utilities/postgresql-client/install.sh
+++ scripts/utilities/postgresql-client/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 if ! which psql > /dev/null; then
-  wget --quiet --output-document - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
-  | gpg --dearmor \
-  | sudo tee /usr/share/keyrings/postgresql-archive-keyring.gpg
+  LOCAL_KEYRING_GPG_FILE="/usr/share/keyrings/postgresql-archive-keyring.gpg"
 
-  INSTALLED_DISTRIBUTION=$(lsb_release --codename --short 2> /dev/null)
+  wget \
+    --quiet \
+    --output-document - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+  | gpg --dearmor \
+  | sudo tee "${LOCAL_KEYRING_GPG_FILE?}"
+
+  source /etc/os-release
   
   cat <<EOF | sudo tee /etc/apt/sources.list.d/pgdg.list
-deb [signed-by=/usr/share/keyrings/postgresql-archive-keyring.gpg] https://apt.postgresql.org/pub/repos/apt $(lsb_release --codename --short)-pgdg main
+deb [signed-by=${LOCAL_KEYRING_GPG_FILE}] https://apt.postgresql.org/pub/repos/apt ${VERSION_CODENAME}-pgdg main
 EOF
  
   sudo apt-get update -q

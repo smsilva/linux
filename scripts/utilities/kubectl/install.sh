74bc72331325d57a2bf650255b1d5eadee9706be
Silvio Silva <smsilva@gmail.com>
2026-05-27 20:22:14 -0300

    chore(scripts/utilities/kubectl/install.sh): use the latest available version

--- scripts/utilities/kubectl/install.sh
+++ scripts/utilities/kubectl/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+kubectl_version="1.35"
+kubernetes_apt_keyring="/etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+
 # Install using native package management
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
 
@@ -10,25 +13,39 @@ if ! which kubectl > /dev/null; then
     ca-certificates \
     curl
 
+    if [ -f "${kubernetes_apt_keyring?}" ]; then
+      sudo rm -f ${kubernetes_apt_keyring?}
+    else
+      sudo mkdir -p /etc/apt/keyrings
+    fi
+
   # Get Google Cloud Apt Key
   curl \
     --fail \
     --silent \
     --show-error \
     --location \
-    "https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key" \
+    "https://pkgs.k8s.io/core:/stable:/v${kubectl_version?}/deb/Release.key" \
   | sudo gpg \
     --dearmor \
-    --output /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    --output "${kubernetes_apt_keyring?}"
 
-  sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+  sudo chmod 644 "${kubernetes_apt_keyring?}"
 
   # Add Kubernetes Repository
   cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /
+deb [signed-by=${kubernetes_apt_keyring?}] https://pkgs.k8s.io/core:/stable:/v${kubectl_version?}/deb/ /
 EOF
 
-  sudo apt-get update -qqq
+  sudo apt-get update -qq
 
-  sudo apt-get install kubectl --yes -q
+  last_available_kubectl_version=$(
+    apt-cache madison kubectl \
+      | grep pkgs.k8s.io \
+      | awk '{ print $3 }' \
+      | sort --version-sort \
+      | tail -1
+  )
+
+  sudo apt-get install kubectl=${last_available_kubectl_version?} --yes -q
 fi
